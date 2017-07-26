c024c68bffd373dfbfc42eda66415e4e5c4d7036
Syed Muhammad Taha <syed.taha@RT-LT-227.rolustech.net>
2017-07-26 11:02:28 +0500

    first commit on College Project

---
+++ Extension/application/Ext/Include/College.php
@@ -0,0 +1,13 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['clg_Student'] = 'clg_Student';
+$beanFiles['clg_Student'] = 'modules/clg_Student/clg_Student.php';
+$moduleList[] = 'clg_Student';
+$beanList['clg_Course'] = 'clg_Course';
+$beanFiles['clg_Course'] = 'modules/clg_Course/clg_Course.php';
+$moduleList[] = 'clg_Course';
+$beanList['clg_Teacher'] = 'clg_Teacher';
+$beanFiles['clg_Teacher'] = 'modules/clg_Teacher/clg_Teacher.php';
+$moduleList[] = 'clg_Teacher';
+
+?>
